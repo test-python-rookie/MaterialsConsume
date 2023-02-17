d464d227024cf1ba571ca76579e13216f4b9ff06
dong <dong@admin.com>
2023-02-17 15:38:03 +0800

    添加登录数据

---
+++ data/data_login.py
@@ -0,0 +1,23 @@
+# 登录参数
+class DataLogin:
+    username = 'wmadmin'
+    password = ('123456', '1234')
+    expect = 'http://47.92.237.67/materialsconsume/#/index'
+    errorname = 'login'
+
+    # # 账号
+    # def login_username(self):
+    #     self.username = 'wmadmin'
+    #     return self.username
+    # # 密码
+    # def login_password(self):
+    #     self.password = '1234567'
+    #     return self.password
+    # # 断言比较
+    # def login_expect(self):
+    #     self.expect = 'http://47.92.237.67/materialsconsume/#/index'
+    #     return self.expect
+    # # 错误截图名称
+    # def login_errorname(self):
+    #     self.errorname = 'login'
+    #     return self.errorname
